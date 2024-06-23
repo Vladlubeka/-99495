bb033a60da0bacf04f6388338371330b7e87bde1
Vladlubeka <128980992+Vladlubeka@users.noreply.github.com>
2024-06-23 19:00:22 +0300

    Add files via upload

---
+++ ДЗ5932.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace GraphPlotter
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.btnDrawGraph = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnDrawGraph
+            // 
+            this.btnDrawGraph.Location = new System.Drawing.Point(12, 12);
+            this.btnDrawGraph.Name = "btnDrawGraph";
+            this.btnDrawGraph.Size = new System.Drawing.Size(75, 23);
+            this.btnDrawGraph.TabIndex = 0;
+            this.btnDrawGraph.Text = "Draw Graph";
+            this.btnDrawGraph.UseVisualStyleBackColor = true;
+            this.btnDrawGraph.Click += new System.EventHandler(this.btnDrawGraph_Click);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(12, 41);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(776, 397);
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.btnDrawGraph);
+            this.Name = "Form1";
+            this.Text = "Graph Plotter";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDrawGraph;
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
---
+++ ДЗ5932.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GraphPlotter
+{
+    public partial class Form1 : Form
+    {
+        private bool shouldDrawGraph = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDrawGraph_Click(object sender, EventArgs e)
+        {
+            shouldDrawGraph = true;
+            pictureBox.Invalidate(); 
+        }
+
+        private void pictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            if (shouldDrawGraph)
+            {
+                Graphics g = e.Graphics;
+                g.Clear(Color.White);
+
+                Pen axisPen = new Pen(Color.Black, 2);
+                g.DrawLine(axisPen, 0, pictureBox.Height / 2, pictureBox.Width, pictureBox.Height / 2); 
+                g.DrawLine(axisPen, pictureBox.Width / 2, 0, pictureBox.Width / 2, pictureBox.Height); 
+
+                Pen functionPen = new Pen(Color.Blue, 2);
+                for (int x = -pictureBox.Width / 2; x < pictureBox.Width / 2; x++)
+                {
+                    int y = x; 
+                    int screenX = x + pictureBox.Width / 2;
+                    int screenY = -y + pictureBox.Height / 2;
+                    if (screenX >= 0 && screenX < pictureBox.Width && screenY >= 0 && screenY < pictureBox.Height)
+                    {
+                        g.DrawEllipse(functionPen, screenX, screenY, 1, 1); 
+                    }
+                }
+            }
+        }
+    }
+}
